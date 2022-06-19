ce1ad98dbcc1f699a1b88eea28ad55812c70b8fa
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-06-19 16:25:10 +0100

    first task (from lesson)

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
 
+url = "https://2ip.ru/"
 
-if __name__ == '__main__':
-    pass
+responce = requests.get(url)
+text = responce.text
+
+soup = bs4.BeautifulSoup(text, features="html.parser")
+# print(soup)
+ip_addr = soup.find(id="d_clip_button").find('span').text
+print(ip_addr)
+
+# if __name__ == '__main__':
+#     pass
