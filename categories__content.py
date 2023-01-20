bf1bbbb13cab4e120fc9d980f511d9097836374b
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-01-20 17:35:49 +0000

    added: Flask

--- categories__content.py
+++ categories__content.py
@@ -1,4 +1,6 @@
 import json
+from pprint import pprint
+from time import sleep
 
 import requests
 import bs4
@@ -31,3 +33,78 @@ def get_categories(href: str) -> dict:
         urls_list.setdefault(name, url)
 
     return urls_list
+
+
+def get_product_in_category(category_name: str) -> dict:
+
+    with open("products_list.json", 'r') as f:
+        categories_list = f.read()
+
+    # current_category = categories_list['Bodysuits']
+    #
+    # print(f'current_category: {current_category}')
+
+    return categories_list
+
+
+def get_all_products(product_categories: list) -> dict:
+    products_list = []
+    headers = read_headers()
+
+    with open("products_list.json", 'w') as f:
+        json.dump({}, f, ensure_ascii=False)
+
+    for category_name, href in product_categories.items():
+
+        product_list = []
+
+        sleep(0.5)
+
+        response = requests.get(href, headers=headers)
+        text = response.text
+        soup = bs4.BeautifulSoup(text, features="html.parser")
+
+        data = soup.find('div', class_="product-grid__items css-hvew4t")
+
+        products = data.find_all('div',
+                                 class_="product-card product-grid__card css-c2ovjx")
+
+        for product in products:
+            product_cart = product.find('div',
+                                        class_='product-card__body')
+            product_img = product_cart.find('a',
+                                            class_='product-card__img-link-overlay')
+            aria_label = product_img.get('aria-label')
+            href = product_img.get('href')
+            product_cart_price_info = product_cart.find('div',
+                                                        class_='product-card__price')
+
+            price = ()
+            try:
+                product_price = product_cart_price_info.get_text().split("€")
+                price = ''.join([i.replace(u'\xa0', u"€ ") for i in product_price])
+            except AttributeError:
+                price = '-'
+                print('NoneType. price = "-"')
+
+            product_dict = {'name': aria_label.replace(u'\xe4', u' '),
+                            'url': href.replace(u'\xe4', u' '),
+                            'price': price.replace(u'\xe4', u' ')}
+
+            product_list.append(product_dict)
+
+        current_category = {category_name: product_list}
+
+        products_list.append(current_category)
+
+        with open("products_list.json", 'a', encoding='utf-8') as f:
+            try:
+                json.dump(current_category, f, ensure_ascii=False, indent=3)
+            except Exception as inst:
+                print('Error record to json:')
+                pprint(current_category)
+                print(type(inst))  # the exception instance
+                print(inst.args)  # arguments stored in .args
+                print(inst)
+
+    return products_list
--- main.py
+++ main.py
@@ -1,70 +1,53 @@
-import json
-
-import requests
-import bs4
-from time import sleep
-
+from flask import Flask, jsonify
+from flask.views import View
 from pprint import pprint
 
-from categories__content import read_headers, get_categories
+from categories__content import read_headers, get_categories, get_all_products, get_product_in_category
 
-HEADERS = read_headers()
+# product_categories = get_categories(base_url)
+#
+# pprint(get_all_products(product_categories))
 
-base_url = "https://www.nike.com/de/w/damen-gym-running-22fovz5e1x6"
+app = Flask(__name__)
 
-product_categories = get_categories(base_url)
 
-products_list = []
+class Base(View):
+    def get(self, user_id: int):
+        return jsonify({'Hello': 'Men!'})
 
-for category_name, href in product_categories.items():
 
-    product_list = []
+@app.route('/')
+def hello():
+    return jsonify({'Hello': 'Men!'})
 
-    sleep(0.5)
 
-    response = requests.get(href, headers=HEADERS)
-    text = response.text
-    soup = bs4.BeautifulSoup(text, features="html.parser")
+# @app.route('/<name>')
+# def hello_name(name):
+#     print(f'name: {name}')
+#     return jsonify({'Hello': name})
 
-    data = soup.find('div', class_="product-grid__items css-hvew4t")
 
-    products = data.find_all('div',
-                             class_="product-card product-grid__card css-c2ovjx")
+@app.route('/get_categories')
+def get_categories_view():
+    base_url = "https://www.nike.com/de/w/damen-gym-running-22fovz5e1x6"
+    product_categories = get_categories(base_url)
+    return jsonify(product_categories)
 
-    for product in products:
-        product_cart = product.find('div',
-                                    class_='product-card__body')
-        product_img = product_cart.find('a',
-                                        class_='product-card__img-link-overlay')
-        aria_label = product_img.get('aria-label')
-        href = product_img.get('href')
-        product_cart_price_info = product_cart.find('div',
-                                                    class_='product-card__price')
 
-        price = ()
-        try:
-            product_price = product_cart_price_info.get_text().split("€")
-            price = ''.join([i.replace(u'\xa0', u"€ ") for i in product_price])
-        except AttributeError:
-            price = '-'
-            print('NoneType. price = "-"')
+@app.route('/get_all_products')
+def get_all_products_view():
+    base_url = "https://www.nike.com/de/w/damen-gym-running-22fovz5e1x6"
+    product_categories = get_categories(base_url)
+    all_products = get_all_products(product_categories)
+    return jsonify(all_products)
 
-        product_dict = {'name': aria_label.replace(u'\xe4', u' '),
-                        'url': href.replace(u'\xe4', u' '),
-                        'price': price.replace(u'\xe4', u' ')}
+# http://127.0.0.1:5000/get_products_by_category/Bodysuits
+@app.route('/get_products_by_category/<name>/')
+def get_product_by_category_view(name):
+    # return jsonify({'Hello': name})
+    products = get_product_in_category(name)
+    return jsonify(products)
 
-        product_list.append(product_dict)
 
-    current_category = {category_name: product_list}
-
-    products_list.append(current_category)
-
-    with open("products_list.json", 'a', encoding='utf-8') as f:
-        try:
-            json.dump(current_category, f, ensure_ascii=False, indent=3)
-        except Exception as inst:
-            print('Error record to json:')
-            pprint(current_category)
-            print(type(inst))  # the exception instance
-            print(inst.args)  # arguments stored in .args
-            print(inst)
+if __name__ == '__main__':
+    app.run()
