f4cb987e46d5f66132878710c6b44b44ce82ea97
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-02-20 17:31:02 +0000

    changed: get_product_data

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import requests
 from pprint import pprint
 
-url = 'http://127.0.0.1:5000'
+# url = 'http://127.0.0.1:5000'
+url = 'https://scrnk190122-production.up.railway.app/'
 
 request = requests.post(f'{url}/product',
                         json={"url": 'https://www2.hm.com/pt_pt/productpage.1137275001.html',
