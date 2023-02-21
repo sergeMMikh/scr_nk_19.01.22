06ddb3dfffcaabb81adc08ada9db9121b0bd0a1f
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-02-21 19:26:11 +0000

    added: price to simple_content.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 
-url = 'http://127.0.0.1:5000'
+# url = 'http://127.0.0.1:5000'
 url = 'https://scrnk190122-production.up.railway.app/'
 
 request = requests.post(f'{url}/product',
