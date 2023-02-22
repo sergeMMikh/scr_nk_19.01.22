a03a8df897eca4dd00c8ceeb5eb0d48bfc3620cd
SMMikhalev <s.m.mih@mail.ru>
2023-02-22 15:03:07 +0000

    adjusted for amazon

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import requests
 from pprint import pprint
 
-url = 'http://127.0.0.1:5000'
-# url = 'https://scrnk190122-production.up.railway.app/'
+# url = 'http://127.0.0.1:5000'
+url = 'https://scrnk190122-production.up.railway.app/'
 
 request = requests.post(f'{url}/product',
                         json={"url": 'https://www.amazon.es/Tommy-Hilfiger-Modern-Zapatillas-Desierto/dp/B09QB4FM7Y/ref=sr_1_1_sspa?crid=2FR01AQTOH8X0&keywords=sapatos+homem+casual&qid=1677060420&sprefix=sapatos%2Caps%2C157&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1',
--- content/simple_content.py
+++ content/simple_content.py
@@ -1,5 +1,4 @@
 import re
-# from pprint import pprint
 
 import requests
 import bs4
